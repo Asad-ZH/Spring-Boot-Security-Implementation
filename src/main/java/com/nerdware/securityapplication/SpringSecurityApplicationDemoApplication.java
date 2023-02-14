63836b6350c3671155000cd1cada281baf6e6f53
asad <asadzahid1479@gmail.com>
2023-02-14 18:51:19 +0500

    setup environment

---
+++ src/main/java/com/nerdware/securityapplication/SpringSecurityApplicationDemoApplication.java
@@ -0,0 +1,13 @@
+package com.nerdware.securityapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityApplicationDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSecurityApplicationDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nerdware/securityapplication/SpringSecurityApplicationDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nerdware.securityapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityApplicationDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
